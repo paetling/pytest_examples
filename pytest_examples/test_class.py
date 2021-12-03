4ad02d15196854d90fdf106f13fedf6a6b733a0d
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2021-12-03 14:22:32 -0500

    this is the multiply example test

--- pytest_examples/test_class.py
+++ pytest_examples/test_class.py
@@ -29,3 +29,39 @@ class TestAdd:
     def test_add_floats_and_int(self):
         instance = ClassToTest()
         assert instance.add(1, 2.1) == pytest.approx(3.1)
+
+
+class TestMultiple:
+    def test_multiple_string(self):
+        instance = ClassToTest()
+        with pytest.raises(TypeError):  
+            instance.multiply("2", "3")  
+
+    def test_multiple_arrays(self):
+        instance = ClassToTest()
+        with pytest.raises(TypeError): 
+            instance.multiply([3, 4, 5], 1)
+
+    def test_multiple_ints(self):
+        instance = ClassToTest()
+        assert instance.multiply(2, 3) == 6
+        assert instance.multiply(3, 3) == 9
+        assert instance.multiply(6, 2) == 12
+
+    def test_multiple_floats(self):
+        instance = ClassToTest()
+        instance.multiply(2.1, 3.2) 
+        assert instance.multiply(1.2, 2.1) == pytest.approx(2.52)
+        assert instance.multiply(-1.1, 2.1) == pytest.approx(-2.31)
+        assert instance.multiply(0.0, 1.5) == pytest.approx(0)
+
+    def test_multiple_floats_and_int(self): 
+        instance = ClassToTest()
+        assert instance.multiply(1, 2.1) == pytest.approx(2.1)      
+
+
+
+
+
+
+
